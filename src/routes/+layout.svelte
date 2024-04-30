9c5c11ea6b1ac735637fb3ac2e5014081e6d43b8
aadamr0 <adamb.0@outlook.com>
2024-04-30 12:04:01 +0100

    barebones page transition implementation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,22 +1,25 @@
 <script lang="ts">
 	import { fade } from 'svelte/transition';
 	export let data;
-    const transitionSpeed = 250/2
+	const transitionSpeed = 250 / 2;
 </script>
 
 <nav>
-    <ul>
-        <li>
-            <a href="/">home</a>
-        </li>
-        <li>
-            <a href="/about">about</a>
-        </li>
-    </ul>
+	<ul>
+		<li>
+			<a href="/">home</a>
+		</li>
+		<li>
+			<a href="/about">about</a>
+		</li>
+	</ul>
 </nav>
 {#key data}
-	<div in:fade={{duration: transitionSpeed, delay: transitionSpeed + 250}} out:fade={{duration: transitionSpeed}}>
-        {data.url}
+	<div
+		in:fade={{ duration: transitionSpeed, delay: transitionSpeed + 250 }}
+		out:fade={{ duration: transitionSpeed }}
+	>
+		{data.url}
 		<slot />
 	</div>
 {/key}
